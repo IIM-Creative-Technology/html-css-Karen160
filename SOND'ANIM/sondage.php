7be5cefe4fadb8fe8fb350b586463eac1ec6b4f6
karen azoulay <=>
2020-09-30 17:05:05 +0200

    karen

--- SOND'ANIM/sondage.php
+++ SOND'ANIM/sondage.php
@@ -1,6 +1,6 @@
 <?php include('inc/head.php'); //Nav ?>
     <title>Sond'Anim</title>
-    <meta name="description" content="Répondez aux différents sondages concernant vos animés préférés et gagnez des points pour devenir premier au classement !">
+    <meta name="description" content="Répondez aux différents sondages concernant vos animés préférés proposez ici ! Les sondages de Démon Slayer, Hunter x Hunter, My hero academia et One piece vont vous plaire !">
 <head>
 <body>
     <header>
@@ -36,4 +36,16 @@
                         <h2>Démon Slayer</h2>
                     </a>
                 </div>
-            </div>
+        </div>
+    </main>
+
+<footer>
+    <p>Site web crée par Karen Azoulay dans le cadre d'un projet scolaire</p>
+    <br>
+    <div>
+        <img src="img/logo/iim.png" alt="Logo IIM">
+        <img src="img/logo/pole.png" alt="Logo Pole léonard de vinci">
+    </div>     
+</footer>
+</body>
+</html>
